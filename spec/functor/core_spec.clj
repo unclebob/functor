21c85419074cb8953e4d0d7abded7c92735025a6
Robert C. Martin <unclebob@cleancoder.com>
2024-01-28 14:09:05 -0600

    initial commit

---
+++ spec/functor/core_spec.clj
@@ -0,0 +1,84 @@
+(ns functor.core-spec
+  (:require [functor.core :refer :all]
+            [speclj.core :refer :all]))
+
+(describe "functor"
+  (it "calculates mean"
+    (should= 6 (mean [3 6 9]))))
+
+(describe "functor-macro"
+  (it "generates empty functor"
+    (should=
+      '(clojure.core/fn [x])
+      (macroexpand-1 '(functor ([x])))))
+
+  (it "generates degenerate functor"
+    (should=
+      '(clojure.core/fn [x] 1)
+      (macroexpand-1 '(functor ([x] 1)))))
+
+  (it "generates functor with one degenerate subfunction"
+    (should=
+      '(clojure.core/fn [x]
+         (clojure.core/let [g (clojure.core/fn [this y] this)
+                            this {}]
+           1))
+      (macroexpand-1 '(functor ([x] 1) (g [y])))))
+
+  (it "generates functor with two degenerate methods and some calls"
+    (should=
+      '(clojure.core/fn [x]
+         (clojure.core/let [g (clojure.core/fn [this y] this)
+                            h (clojure.core/fn [this z] this)
+                            this {}
+                            this (g this 1)
+                            this (h this 2)]
+           this))
+      (macroexpand-1 '(functor ([x] (g 1) (h 2) this)
+                               (g [y])
+                               (h [z])))))
+
+  (it "generates functor with a setter method and a call"
+    (should=
+      '(clojure.core/fn [x]
+         (clojure.core/let
+           [g (clojure.core/fn [this y]
+                (clojure.core/let
+                  [this (clojure.core/assoc this :v y)]
+                  this))
+            this {}
+            this (g this 1)]
+           v))
+      (macroexpand-1 '(functor ([x] (g 1) v)
+                               (g [y] (<- v y))))))
+  )
+
+(describe "functor utilities"
+  (context "expanding functor body"
+    (it "expands a degenerate body"
+      (should= [[] [1]] (expand-functor-body {} [1])))
+    (it "should expand method calls and body"
+      (should= [['this '(g this 1)] ['this]]
+               (expand-functor-body {:method-names #{'g}} ['(g 1) 'this])))
+    )
+
+  (context "expanding methods"
+    (it "expands degenerate method"
+      (should= ['this #{}] (expand-method-body {} [])))
+
+    (it "should expand a setter"
+      (should= ['(clojure.core/let [this (clojure.core/assoc this :v x)] this)
+                #{'v}]
+               (expand-method-body {} ['(<- v x)])))
+    )
+  )
+
+(def degenerate (functor ([] 1)))
+(def one-method (functor ([] (g 2) this) (g [x] (<- v x))))
+
+(prn 'one-method (macroexpand-1 '(functor ([] (g 2) this) (g [x] (<- v x)))))
+
+(describe "functor execution"
+  (it "executes functors"
+    (should= 1 (degenerate))
+    (should= {:v 2} (one-method))))
---
+++ src/functor/core.clj
@@ -0,0 +1,107 @@
+(ns functor.core
+  (:require [clojure.set :as set]))
+
+;(def mean (functor ([ns]
+;  (make-sum)
+;  (/ sum (count ns)))
+; (make-sum []
+;  (<- sum (reduce + ns)))
+;  )
+
+(def mean
+  (fn [ns]
+    (let [make-sum (fn [this]
+                     (assoc this :sum (reduce + ns)))
+          this {}
+          {:keys [sum] :as this} (make-sum this)]
+      (/ sum (count ns)))))
+
+
+(defn expand-method-body [method-data method-body]
+  (if (empty? method-body)
+    ['this #{}]
+    (loop [forms method-body
+           expanded-forms []
+           vars #{}]
+      (cond
+        (empty? forms)
+        (let [final-let `(let [~@expanded-forms] ~'this)]
+          [final-let vars])
+
+        (not (seq? (first forms)))
+        (throw (Exception. (prn-str "not a seq:" (first forms))))
+
+        (= '<- (ffirst forms))
+        (let [form (first forms)
+              var (second form)
+              body (drop 2 form)]
+          (recur (rest forms)
+                 (concat expanded-forms ['this `(assoc ~'this ~(keyword var) ~@body)])
+                 (conj vars var)))
+
+        :else
+        (throw (Exception. (prn-str "horrible: " (first forms))))
+        ))))
+
+(defn- expand-method [method-desc method-data]
+  (let [name (first method-desc)
+        args (vec (concat ['this] (second method-desc)))
+        body (drop 2 method-desc)
+        [body vars] (expand-method-body method-data body)
+        method-data (update method-data :methods concat [name `(fn ~args ~body)])
+        method-data (update method-data :method-names conj name)
+        method-data (update method-data :vars set/union vars)]
+    method-data))
+
+(defn- make-lets [method-expansions]
+  (vec (concat [] method-expansions ['this {}])))
+
+(defn expand-functor-body [{:keys [method-names] :as method-data} body-forms]
+  (loop [forms body-forms
+         method-calls []
+         body-calls []]
+    (cond
+      (empty? forms)
+      [method-calls body-calls]
+
+
+      (not (seq? (first forms)))
+      (recur (rest forms)
+             method-calls
+             (conj body-calls (first forms)))
+
+      (method-names (ffirst forms))
+      (let [form (first forms)
+            name (first form)
+            args (rest form)]
+        (recur (rest forms)
+               (concat method-calls ['this `(~name ~'this ~@args)])
+               body-calls))
+
+      :else
+      (recur (rest forms)
+             method-calls
+             (concat body-calls (first body-forms)))
+      )))
+
+(defn- generate-functor [method-data functor-desc methods-desc]
+  (let [arg-list (first functor-desc)
+        body (rest functor-desc)]
+    (if (some? methods-desc)
+      (let [lets (make-lets (:methods method-data))
+            [method-calls functor-body] (expand-functor-body method-data body)
+            lets (vec (concat lets method-calls))]
+        `(fn ~arg-list (let
+                         ~lets
+                         ~@functor-body)))
+      `(fn ~arg-list ~@body))
+    ))
+
+(defmacro functor [functor-desc & methods-desc]
+  (loop [methods methods-desc
+         method-data {:methods [] :method-names #{} :vars #{}}]
+    (if (empty? methods)
+      (generate-functor method-data functor-desc methods-desc)
+      (recur (rest methods)
+             (expand-method (first methods) method-data))
+      )))
